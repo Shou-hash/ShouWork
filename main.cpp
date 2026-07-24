8e7fc4131dbcc482f084804e928a153e8ead835b
Shou <k025g0060@g.neec.ac.jp>
2026-07-24 14:45:46 +0900

    最新版

--- main.cpp
+++ main.cpp
@@ -1330,6 +1330,7 @@ int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE, _In_ LPSTR, _In
 	CloseHandle(fenceEvent);
 	CloseWindow(hwnd);
 
+
 #ifdef USE_IMGUI
 	ImGui_ImplDX12_Shutdown();
 	ImGui_ImplWin32_Shutdown();
